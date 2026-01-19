4b6a269e33d4e440686430e8f3f68f6f585fc5e4
Gseynave <gauthier.seynave@gmail.com>
2026-01-19 17:39:32 +0100

    Initial site setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'From Shade';
+export const SITE_DESCRIPTION = 'A backend developer moving forward the light';
